824cf51aef499f2f4ef5c60577b4a915e0431ab0
anandamarwan <anandapuspita2@gmail.com>
2024-06-12 19:16:39 +0700

    feat:change apps to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MainApp } from "./apps";
+import { MainApp } from "./app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
