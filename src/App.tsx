0044a641e83705959b5d7ce9e75b92a0fbc0cfe3
anandamarwan <anandapuspita2@gmail.com>
2024-06-12 14:49:33 +0700

    feat: fix const mainApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Activity, dataActivities } from "./data/activities";
 import { ActivityItem } from "./components/activity-item";
 
-const MainApp = () => {
+export const MainApp = () => {
   return (
     <div>
       <main className="m-10 flex justify-center">
@@ -23,5 +23,3 @@ const MainApp = () => {
     </div>
   );
 };
-
-export { MainApp };
