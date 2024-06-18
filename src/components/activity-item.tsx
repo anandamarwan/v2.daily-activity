bb2680a820d61fa2bba3641b10d5943b4863b946
anandamarwan <anandapuspita2@gmail.com>
2024-06-18 14:31:12 +0700

    feat: update styling

--- src/components/activity-item.tsx
+++ src/components/activity-item.tsx
@@ -5,7 +5,6 @@ export function ActivityItem({ activity }: { activity: Activity }) {
     <div className="p-2">
       <p>{activity.title}</p>
       <p className="text-slate-400">{activity.category}</p>
-      <hr />
     </div>
   );
 }
--- src/routes/activities.tsx
+++ src/routes/activities.tsx
@@ -54,7 +54,7 @@ export function ActivitiesRoute() {
                 defaultValue="food"
                 id="category"
                 name="category"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-40 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option value="food">Food</option>
                 <option value="sport">Sport</option>
